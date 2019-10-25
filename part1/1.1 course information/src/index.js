a281e9d9cb78c0b5683fba407b00c175482fa5d1
Maslov Pavel <maslov.pav@bk.ru>
2019-10-25 16:52:35 +0300

    1.4 done

--- part1/1.1 course information/src/index.js
+++ part1/1.1 course information/src/index.js
@@ -36,25 +36,26 @@ const Footer = (props) => {
 
 const App = () => {
     const course = 'Half Stack application development';
-    const part1 = {
-        name: 'Fundamentals of React',
-        exercises: 10
-    }
-    const part2 = {
-        name: 'Using props to pass data',
-        exercises: 7
-    }
-    const part3 = {
-        name: 'State of a component',
-        exercises: 14
-    }
-    let contentProps = [part1, part2, part3];
+    let parts = [
+        {
+            name: 'Fundamentals of React',
+            exercises: 10
+        },
+        {
+            name: 'Using props to pass data',
+            exercises: 7
+        },
+        {
+            name: 'State of a component',
+            exercises: 14
+        }
+    ];
 
     return (
         <div>
             <Header courseName={course}/>
-            <Content arr={contentProps}/>
-            <Footer exercisesCnt={part1.exercises + part2.exercises  + part3.exercises }/>
+            <Content arr={parts}/>
+            <Footer exercisesCnt={parts.reduce((acc, part) => acc + part.exercises, 0)}/>
         </div>
     );
 }
