0e8dc48736bf5069dd1b03a3ee8674ecf2955dd0
Maslov Pavel <maslov.pav@bk.ru>
2019-10-25 16:31:07 +0300

    step3 done

--- part1/1.1 course information/src/index.js
+++ part1/1.1 course information/src/index.js
@@ -19,9 +19,9 @@ const Part = (props) => {
 const Content = (props) => {
     return (
         <div>
-            <Part partName={props.arr[0].name} exercisesCnt={props.arr[0].cnt}/>
-            <Part partName={props.arr[1].name} exercisesCnt={props.arr[1].cnt}/>
-            <Part partName={props.arr[2].name} exercisesCnt={props.arr[2].cnt}/>
+            <Part partName={props.arr[0].name} exercisesCnt={props.arr[0].exercises}/>
+            <Part partName={props.arr[1].name} exercisesCnt={props.arr[1].exercises}/>
+            <Part partName={props.arr[2].name} exercisesCnt={props.arr[2].exercises}/>
         </div>
     )
 }
@@ -36,32 +36,25 @@ const Footer = (props) => {
 
 const App = () => {
     const course = 'Half Stack application development';
-    const part1 = 'Fundamentals of React';
-    const exercises1 = 10;
-    const part2 = 'Using props to pass data';
-    const exercises2 = 7;
-    const part3 = 'State of a component';
-    const exercises3 = 14;
-    let contentProps = [
-        {
-            name: part1,
-            cnt: exercises1
-        },
-        {
-            name: part2,
-            cnt: exercises2
-        },
-        {
-            name: part3,
-            cnt: exercises3
-        }
-    ];
+    const part1 = {
+        name: 'Fundamentals of React',
+        exercises: 10
+    }
+    const part2 = {
+        name: 'Using props to pass data',
+        exercises: 7
+    }
+    const part3 = {
+        name: 'State of a component',
+        exercises: 14
+    }
+    let contentProps = [part1, part2, part3];
 
     return (
         <div>
             <Header courseName={course}/>
             <Content arr={contentProps}/>
-            <Footer exercisesCnt={exercises1 + exercises2 + exercises3}/>
+            <Footer exercisesCnt={part1.exercises + part2.exercises  + part3.exercises }/>
         </div>
     );
 }
