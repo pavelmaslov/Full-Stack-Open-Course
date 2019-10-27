20c7b104c166a61fe1d1d38bedd56ef55f92e7d4
Maslov Pavel <maslov.pav@bk.ru>
2019-10-27 15:58:21 +0300

    1.6 done

---
+++ part1/1.2-unicafe/src/index.js
@@ -0,0 +1,54 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = ({text}) => {
+    return (
+        <h1>
+            {text}
+        </h1>
+    );
+}
+
+const Button = ({onClickHandler, text}) => {
+    return (
+        <button onClick={onClickHandler}>
+            {text}
+        </button>
+    );
+}
+
+const Display = ({text, value}) => {
+    return (
+        <div>
+            {text} {value}
+        </div>
+    );
+}
+
+const App = () => {
+    // save clicks of each button to own state
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
+
+    const addScore = (newVal, setFunc) => {
+        return () => setFunc(newVal);
+    };
+
+    return (
+    <div>
+        <Header text="give feedback"/>
+        <Button onClickHandler={addScore(good + 1, setGood)} text="good"/>
+        <Button onClickHandler={addScore(neutral + 1, setNeutral)} text="neutral"/>
+        <Button onClickHandler={addScore(bad + 1, setBad)} text="bad"/>
+        <Header text="statistics"/>
+        <Display text="good" value={good}/>
+        <Display text="neutral" value={neutral}/>
+        <Display text="bad" value={bad}/>
+    </div>
+    )
+}
+
+ReactDOM.render(<App />, 
+    document.getElementById('root')
+)
