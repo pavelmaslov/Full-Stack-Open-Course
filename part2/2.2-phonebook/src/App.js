ad8aa1233ae1c8f610099e3bdbc852b8ebb95197
Maslov Pavel <maslov.pav@bk.ru>
2020-02-26 15:32:04 +0300

    2.11 done

--- part2/2.2-phonebook/src/App.js
+++ part2/2.2-phonebook/src/App.js
@@ -1,19 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Persons from './Persons';
 import Filter from './Filter';
 import PersonForm from './PersonForm';
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    {name: 'Arto Hellas', number: '040-123456', id: 1},
-    {name: 'Ada Lovelace', number: '39-44-5323523', id: 2},
-    {name: 'Dan Abramov', number: '12-43-234345', id: 3},
-    {name: 'Mary Poppendieck', number: '39-23-6423122', id: 4}
-  ]);
+  const [ persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [searchString, setSerchString] = useState('');
   const personsToShow = searchString === '' ? persons : persons.filter(person => person.name.toLowerCase().includes(searchString.toLowerCase()));
+
+  const initialPersonsLoadHook = () => {
+    const personsUrl = 'http://localhost:3001/persons';
+    axios
+      .get(personsUrl)
+      .then(res => {
+        setPersons(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  useEffect(initialPersonsLoadHook, []);
 
   const nameExists = (name) => {
     return persons.filter(person => person.name === name).length > 0;
